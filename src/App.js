6ecef23b020d4c03250a0bccce37d8c47037760c
Edward DiDonato <edward.didonato@gmail.com>
2020-10-09 14:43:45 -0400

    Inventory Basic Functioning Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Inventory from './Inventory';
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Inventory />
     </div>
   );
 }
---
+++ src/Inventory.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./App.css";
+
+function Inventory(props) {
+  const [spirits, setSpirits] = useState([])
+  // const [price, setPrice] = useState([])
+
+  useEffect(() => {
+
+    const getInventory = async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/spirits`;
+      const response = await axios.get(airtableURL, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      setSpirits(response.data.records)
+    };
+    getInventory();
+  }, [])
+
+  const price = spirits.map((spirit) => (spirit.fields.price))
+  // const bottleSize = spirits.map((spirit) => (spirit.fields.bottleSizes))
+  const amountFull = spirits.map((spirit) => spirit.fields.amountFull)
+  const totalInventory = (price, amountFull) => {
+    let total = 0;
+    for (let i = 0; i < spirits.length; i++) {
+      total += price[i] * amountFull[i]
+    }
+    return total
+  }
+  console.log(totalInventory(price, amountFull))
+
+  return (
+    <div>
+      <h1>Inventory</h1>
+      <h2>
+        total inventory: ${totalInventory(price, amountFull)}
+      </h2>
+      <div className="spreadsheet">
+        <div className="spirits-column">
+          <h3>Spirit</h3>
+          {spirits.map((spirit) => (
+            <p key={spirit.id}>{spirit.fields.bottle}</p>
+          ))}
+        </div>
+        <div className="category-column">
+          <h3>Category</h3>
+          {spirits.map((spirit) => (
+            <p key={spirit.id}>{spirit.fields.category}</p>
+          ))}
+        </div>
+        <div className="price-column">
+          <h3>Price</h3>
+          {spirits.map((spirit) => (
+            <p key={spirit.id}>${spirit.fields.price}</p>
+          ))}
+        </div>
+        <div className="size-column">
+          <h3>Bottle Size (mL)</h3>
+          {spirits.map((spirit) => (
+            <p key={spirit.id}>{spirit.fields.bottleSizes}</p>
+          ))}
+        </div>
+        <div className="amount-full-column">
+          <h3>Amount Full</h3>
+          {spirits.map((spirit) => (
+            <p key={spirit.id}>{(spirit.fields.amountFull) * 100}%</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default Inventory; 
