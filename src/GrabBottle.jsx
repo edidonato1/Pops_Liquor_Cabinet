6dcd54ff3029cbd2f2a192956d218a2edf336cb8
Edward DiDonato <edward.didonato@gmail.com>
2020-10-09 17:18:55 -0400

    target drop-down selection for updateBottle

--- src/GrabBottle.jsx
+++ src/GrabBottle.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Route } from 'react-router-dom';
 import axios from 'axios';
-import Inventory from './Inventory';
+// import UpdateBottle from './UpdateBottle'
 
 function GrabBottle(props) {
   const [data, setData] = useState([])
+  const [selection, setSelection] = useState('')
   // const [options, setOptions] = useState([])
 
   useEffect(() => {
@@ -17,25 +18,35 @@ function GrabBottle(props) {
         },
       });
       setData(response.data.records)
-      // setOptions(response.data.records && response.data.records.fields)
 
     };
     getInventory();
 
   }, [])
 
-  // Searching options: 
-  //    // sort the items alphabetically by bottle name 
-  //    //     // populate drop-down with the sorted list
-  //    // use drop-down to select category
-  //    //     // create event listener for onChange that accepts change of <select>
-  //    //     // component.  set the value as the second argument for a second useEffect call
+  useEffect(() => {
 
-  // function getValues(list) {
-  //   return list.map((bottle) => {
-  //     setOptions(bottle)
-  //   })
-  // }
+    const getInventory = async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/spirits`;
+      const response = await axios.get(airtableURL, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      setData(response.data.records)
+
+    };
+    getInventory();
+
+  }, [selection])
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setSelection(e.target.value)
+    console.log(selection)
+  }
+
 
   // alphabetical sorting function from StackOverflow.com
   data.sort(function (a, b) {
@@ -48,14 +59,24 @@ function GrabBottle(props) {
     <div>
       <Route path="/GrabBottle">
         <h1>Grab a Bottle</h1>
-        <select>{
+        <select onChange={handleChange}>{
           data.map((item) =>
             <option key={item.id} value={item.fields.bottle}>{item.fields.bottle}</option>
           )}
         </select>
+        {/* <UpdateBottle bottle={selection.bottle} info={selection.notes} /> */}
       </Route>
     </div>
   )
 }
 
 export default GrabBottle;
+
+
+  // Searching options: 
+  //    // sort the items alphabetically by bottle name 
+  //    //     // populate drop-down with the sorted list
+  //    // use drop-down to select category
+  //    //     // create event listener for onChange that accepts change of <select>
+  //    //     // component.  set the value as the second argument for a second useEffect call
+
