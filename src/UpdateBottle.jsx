175cc95a68a9c832097c56da03baaba0379b3c7a
Edward DiDonato <edward.didonato@gmail.com>
2020-10-11 11:20:50 -0400

    add useEffect to UpdateBottle

--- src/UpdateBottle.jsx
+++ src/UpdateBottle.jsx
@@ -1,48 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 function UpdateBottle(props) {
 
 
   const [amountFull, setAmountFull] = useState(props.bottleData && props.bottleData.amountFull)
-  // const [amountFull, setAmountFull] = useState(props.currentBottle)
 
+  useEffect(() => {
 
-  const handleClick = async () => {
+    const handleClick = async () => {
 
-    const id = props.id;
-    const fields = {
-      amountFull,
+      const id = props.id;
+      const fields = {
+        amountFull,
+      }
+
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/spirits/${id}`;
+      await axios.patch(
+        airtableURL,
+        { fields },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+          },
+        })
     }
-
-    const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/spirits/${id}`;
-    await axios.patch(
-      airtableURL,
-      { fields },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
-        },
-      })
-  }
+    handleClick();
+  }, [amountFull])
 
   const increment = (e) => {
-    console.log(e.target.value)
     setAmountFull(amountFull + .1)
-    handleClick();
-    console.log(amountFull)
   }
   const decrement = () => {
     setAmountFull(amountFull - .1)
-    handleClick();
-    console.log(amountFull)
   }
 
   return (
     <div>
-      {props.bottleData ? <h2>{props.bottleData && props.bottleData.bottle}</h2> : null}
-      {props.bottleData ? <h3> Category: {props.bottleData && props.bottleData.category}</h3> : null}
-      {props.bottleData ? <h3>{(props.bottleData && props.bottleData.amountFull) * 100}% full </h3> : null}
+      {props.bottleData ?
+        <div>
+          <h2>{props.bottleData && props.bottleData.bottle}</h2>
+          <h3> Category: {props.bottleData && props.bottleData.category}</h3>
+          <h3>{(props.bottleData && props.bottleData.amountFull) * 100}% full </h3>
+        </div>
+        : null}
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
     </div>
