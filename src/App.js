6aee48f4b2d9b21f6c4aa6efa71755ccc01f7390
Edward DiDonato <edward.didonato@gmail.com>
2020-10-13 12:17:07 -0400

    update README

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import GrabBottle from './GrabBottle';
 import AddBottle from './AddBottle';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 
 function App() {
@@ -12,10 +13,13 @@ function App() {
   return (
     <div className="App">
       <h1 className="title-tag">Pop's Liquor Cabinet</h1>
+      <h1>fhdjkaslfd</h1>
       <Navbar />
+
       <Inventory
         inventoryRefresh={inventoryRefresh}
         setInventoryRefresh={setInventoryRefresh} />
+
       <GrabBottle
         inventoryRefresh={inventoryRefresh}
         setInventoryRefresh={setInventoryRefresh}
