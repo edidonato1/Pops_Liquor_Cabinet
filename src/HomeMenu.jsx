43a273fbc0cbafc82027b517465e33a99f142b99
Edward DiDonato <edward.didonato@gmail.com>
2020-10-13 23:26:51 -0400

    style scroll menu

--- src/HomeMenu.jsx
+++ src/HomeMenu.jsx
@@ -5,8 +5,8 @@ function HomeMenu() {
   return (
     <div className="scroll-menu">
       <h5 className="scroll-title">about</h5>
-      <p>Pop's Liquor Cabinet is the bar inventory management system for the home enthusiast.
-          Easily add and update inventory, and make tasting notes for yourself as you enjoy your collection.</p>
+      <p>Pop's Liquor Cabinet is the bar inventory management system for the home enthusiast.</p>
+      <p>Easily add and update inventory, and make tasting notes for yourself as you enjoy your collection.</p>
     </div>
 
   )
